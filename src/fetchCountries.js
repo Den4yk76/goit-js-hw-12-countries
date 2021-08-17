0dcbc20d7e0ac144503d882787b97a6ae079b70b
Denys Nazarenko <denysnazarenko23@gmail.com>
2021-08-17 16:46:02 +0300

    working on HW-12 IN PROGRESS

---
+++ src/fetchCountries.js
@@ -0,0 +1,15 @@
+export default class CountriesApiService {
+    constructor() {}
+
+    fetchCountry(e) {
+        const val = e.target.value;
+
+        if (val) {
+            return fetch(`https://restcountries.eu/rest/v2/name/${val}`)
+                .then(response => response.json())
+                .then(data => {
+                    return data;
+                });
+        }
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-import './sass/main.scss';
+// import './sass/main.scss';
+import CountriesApiService from '../src/fetchCountries.js';
+const debounse = require('lodash.debounce');
+
+const refs = {
+    inputEl: document.querySelector('.js-input'),
+    listEl: document.querySelector('.js-countries'),
+};
+
+const countriesApiService = new CountriesApiService();
+
+refs.inputEl.addEventListener('input', debounse(onInput, 1000));
+
+function onInput(e) {
+    refs.listEl.textContent = '';
+    countriesApiService.fetchCountry(e).then(arrayIteration);
+}
+
+function arrayIteration(data) {
+    for (let elem of data) {
+        refs.listEl.insertAdjacentHTML('beforeend', `<li>${elem.name}</li>`);
+    }
+}
